015870d5cb0e59fdb4d282ac512b453395d37e19
M.Sharjeel <muhammadsharjeel819@gmail.com>
2022-07-14 14:52:51 +0500

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
-import React from "react"
+// import React from "react"
 
-export default function Home() {
-  return  <div>Hello World from sharjeel website!</div>
+// export default function Home() {
+//   return  <div>Hello World from sharjeel website!</div>
+// }
+
+
+import React from 'react';
+
+const Home = () => {
+    return(
+        <h1>Hello World</h1>
+    )
 }
 
+export default Home;
