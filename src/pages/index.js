6396770fd8e3e84d7e4f61e7e1c6b8ae0e4e3019
M.Sharjeel <muhammadsharjeel819@gmail.com>
2022-07-15 15:55:54 +0500

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,6 @@ import React from "react"
 
 export default function Home() {
   // return <div>Hey everybody.Welcome to my first frontend pipeline.Thanks---!</div>
-  return <div>Sharjeel from github without building in local directory</div>
+  return <div>Hey fazella with local building</div>
 }
 
